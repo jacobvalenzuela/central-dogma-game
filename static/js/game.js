501c31023e773e1d30209c7e4beb4d7beff4c301
Jeremy Zhang <jzhang@systemsbiology.org>
2019-07-16 17:08:43 -0700

    Sparkles for tactile feedback

--- static/js/game.js
+++ static/js/game.js
@@ -32,6 +32,119 @@
         resizeCanvas();
     });
 
+    class TouchFeedback extends Phaser.Plugins.ScenePlugin {
+        constructor (scene, pluginManager) {
+            super(scene, pluginManager);
+        }
+
+        boot() {
+            this.scene.events.on("start", this.bindFn(this.start));
+        }
+
+        start() {
+            let touchFeedbackPartConf = {
+                x: 0,
+                y: 0,
+                speed: { min: -100, max: 100 },
+                angle: { min: 0, max: 360 },
+                scale: { start: 0.5, end: 0 },
+                blendMode: "SCREEN",
+                active: false,
+                lifespan: 450
+            }
+            this.partGr = this.scene.add.particles("touch_feedback_green_spark").createEmitter(touchFeedbackPartConf);
+            this.partGr.manager.setDepth(10000);
+            this.partYell = this.scene.add.particles("touch_feedback_yellow_spark").createEmitter(touchFeedbackPartConf);
+            this.partYell.manager.setDepth(10000);
+
+            this.scene.input.on("pointerdown", this.bindFn(this.startFeedBack));
+            this.scene.input.on("pointermove", this.bindFn(this.moveFeedBack));
+
+            this.rateLimitHit = false;
+        }
+
+        startFeedBack(inputPlugin, pointer, objClicked) {
+            this.createGrowingCir(pointer.x, pointer.y);
+            this.createParticles(pointer.x, pointer.y, 5);
+        }
+
+        moveFeedBack(inputPlugin, pointer, objHovered) {
+            if (pointer.primaryDown && !this.rateLimitHit) {
+                this.rateLimitHit = true;
+                this.createParticles(pointer.x, pointer.y, 1);
+                this.resetRateLimit();
+            }
+        }
+
+        resetRateLimit() {
+            let that = this;
+            this.scene.time.addEvent({
+                delay: 100,
+                loop: false,
+                callback: function () {
+                    that.rateLimitHit = false;
+                },
+            });
+        }
+
+        createGrowingCir(x, y) {
+            let img = this.scene.add.image(x, y, "touch_feedback_circle").setScale(0.05).setAlpha(0.75).setDepth(10000);
+            this.grow(img);
+            this.fadeOut(img);
+        }
+
+        createParticles(x, y, amt) {
+            this.partGr.setPosition(x, y);
+            this.partYell.setPosition(x, y);
+            this.partGr.resume();
+            this.partYell.resume();
+            this.partGr.explode(amt);
+            this.partYell.explode(amt);
+        }
+
+        grow(img) {
+            if (img.scale >= 0.35) {
+                return;
+            } else {
+                let scale = img.scale + 0.015;
+                img.setScale(scale);
+                let that = this;
+                this.scene.time.addEvent({
+                    delay: 10,
+                    loop: false,
+                    callback: function () {
+                        that.grow(img);
+                    },
+                });
+            }
+        }
+
+        fadeOut(img) {
+            if (img.alpha <= 0) {
+                img.destroy();
+            } else {
+                let alpha = img.alpha - 0.02;
+                img.setAlpha(alpha);
+                let that = this;
+                this.scene.time.addEvent({
+                    delay: 10,
+                    loop: false,
+                    callback: function () {
+                        that.fadeOut(img);
+                    },
+                });
+            }
+        }
+
+        bindFn(fn) {
+            let clas = this;
+            return function (...args) {
+                let event = this;
+                fn.bind(clas, event, ...args)();
+            };
+        }
+    }
+
     class Game {
         constructor (levels) {
             this.config = {
@@ -40,6 +153,16 @@
                 width: 360,
                 height: 740,
                 backgroundColor: "#fff",
+                plugins: {
+                    scene: [
+                        {
+                            key: "TouchFeedback",
+                            plugin: TouchFeedback,
+                            start: true,
+                            mapping: "touchFeedback"
+                        }
+                    ]
+                },
                 scene: {
                     preload: this.bindFn(this.preload),
                     create: this.bindFn(this.create),
@@ -59,6 +182,11 @@
 
         preload(gameObj) {
             this.game = gameObj;
+
+            this.game.load.image("touch_feedback_circle", "static/img/touch_feedback/circle.png");
+            this.game.load.image("touch_feedback_green_spark", "static/img/touch_feedback/green_sparkle.png");
+            this.game.load.image("touch_feedback_yellow_spark", "static/img/touch_feedback/yellow_sparkle.png");
+
             this.game.load.image("logo_dogma", "static/img/DOGMA_logo.png");
             this.game.load.spritesheet(
                 "logo_dogma_intro",
